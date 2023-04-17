2607705b488e7d0919ae82150691f9e2ae2f1c8a
박준형 <108733795+krapli441@users.noreply.github.com>
2023-04-17 19:16:04 +0900

    내가 설명할 수 있는 범위에서만 정답 작성

--- 2023.04.17/exam.js
+++ 2023.04.17/exam.js
@@ -25,14 +25,16 @@ function examTwo(first, second, third) {
 
 function examThree(str) {
   if (typeof str === "string") {
-    console.log("일단 문자열임");
-    let change = new Array(str);
-    console.log(change);
+    let count = 0;
+    while (str[count] !== undefined) {
+      count++;
+    }
+    return count;
   } else {
     console.log("문자열을 입력해주세요.");
   }
   // 문자열만 실행되도록 타입을 확인하는 로직을 구하시오.
-  // l  ength 프로퍼티를 사용하지 않고 길이값을 구하시오.
+  // length 프로퍼티를 사용하지 않고 길이값을 구하시오.
 }
 
 function examFour(array) {
@@ -43,15 +45,27 @@ function examFour(array) {
   // 배열(array)만을 인자로 받아서 실행할 수 있도록 작성하시오.
 }
 
+function examFive(array) {
+  if (Array.isArray(array)) {
+    for (i = 0; i < array.length; i++) {
+      arr;
+    }
+    return array[0] + array[1] + array[2];
+  }
+  // 배열(array)만을 인자로 받아서 실행할 수 있도록 작성하시오.
+}
+
 // function examFive(array) {
 //   if (Array.isArray(array)) {
-//     console.log("배열입니다.");
+//     let count = 0;
+//     array.forEach((item) => (count += item));
+//     return count;
 //   }
 //   // 배열(array)만을 인자로 받아서 실행할 수 있도록 작성하시오.
 // }
 
 console.log(examOne(2, 3)); // Question 1 덧셈 로직 만들기
 console.log(examTwo(2, 3, 4)); //  Question 2 곱셈 로직 만들기
-// console.log(examThree("hello")); // Question 3 문자열 길이 구하기
+console.log(examThree("hello")); // Question 3 문자열 길이 구하기
 console.log(examFour([3, 1, 4, 2])); // Question 4 최솟값 판별하기
-// console.log(examFive([1, 2, 3])); // Question 5 배열값 누산(총합)하기
+console.log(examFive([1, 2, 3])); // Question 5 배열값 누산(총합)하기
