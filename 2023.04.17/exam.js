865c173219eac5a02cbdf469c1d59e394ec790d3
박준형 <108733795+krapli441@users.noreply.github.com>
2023-04-17 18:56:49 +0900

    3번, 5번 막힘

--- 2023.04.17/exam.js
+++ 2023.04.17/exam.js
@@ -32,28 +32,26 @@ function examThree(str) {
     console.log("문자열을 입력해주세요.");
   }
   // 문자열만 실행되도록 타입을 확인하는 로직을 구하시오.
-  // length 프로퍼티를 사용하지 않고 길이값을 구하시오.
+  // l  ength 프로퍼티를 사용하지 않고 길이값을 구하시오.
 }
 
 function examFour(array) {
   if (Array.isArray(array)) {
-    console.log("배열입니다.");
-    for (i = 0; i < array.length; i++) {
-      
-    }
+    let calc = Math.min(...array);
+    return calc;
   }
   // 배열(array)만을 인자로 받아서 실행할 수 있도록 작성하시오.
 }
 
-function examFive(array) {
-  if (Array.isArray(array)) {
-    array.reduce();
-  }
-  // 배열(array)만을 인자로 받아서 실행할 수 있도록 작성하시오.
-}
+// function examFive(array) {
+//   if (Array.isArray(array)) {
+//     console.log("배열입니다.");
+//   }
+//   // 배열(array)만을 인자로 받아서 실행할 수 있도록 작성하시오.
+// }
 
 console.log(examOne(2, 3)); // Question 1 덧셈 로직 만들기
 console.log(examTwo(2, 3, 4)); //  Question 2 곱셈 로직 만들기
-console.log(examThree("hello")); // Question 3 문자열 길이 구하기
+// console.log(examThree("hello")); // Question 3 문자열 길이 구하기
 console.log(examFour([3, 1, 4, 2])); // Question 4 최솟값 판별하기
-console.log(examFive([1, 2, 3])); // Question 5 배열값 누산(총합)하기
+// console.log(examFive([1, 2, 3])); // Question 5 배열값 누산(총합)하기
