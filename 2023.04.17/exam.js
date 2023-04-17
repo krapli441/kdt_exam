4f8ee1f7df5236be70da786ed10bc59503f14ead
박준형 <108733795+krapli441@users.noreply.github.com>
2023-04-17 18:11:44 +0900

    문제 작성

---
+++ 2023.04.17/exam.js
@@ -0,0 +1,27 @@
+function examOne(first, second) {
+  // 매개변수가 문자열일 경우, 다음과 같은 문자열이 나타나도록 작성하시오.
+  // "문자열 데이터를 입력하셨습니다."
+}
+
+function examTwo(first, second, third) {
+  // 숫자여야만 실행되도록 타입을 확인하는 로직을 작성하시오.
+}
+
+function examThree(str) {
+  // 문자열만 실행되도록 타입을 확인하는 로직을 구하시오.
+  // length 프로퍼티를 사용하지 않고 길이값을 구하시오.
+}
+
+function examFour(array) {
+  // 배열(array)만을 인자로 받아서 실행할 수 있도록 작성하시오.
+}
+
+function examFive(array) {
+  // 배열(array)만을 인자로 받아서 실행할 수 있도록 작성하시오.
+}
+
+console.log(examOne(2, 3)); // Question 1 덧셈 로직 만들기
+console.log(examTwo(2, 3, 4)); //  Question 2 곱셈 로직 만들기
+console.log(examThree("hello")); // Question 3 문자열 길이 구하기
+console.log(examFour([3, 1, 4, 2])); // Question 4 최솟값 판별하기
+console.log(examFive([1, 2, 3])); // Question 5 배열값 누산(총합)하기
