6db12d2d6170f13de6b785c8dd128f5c03579cf5
krapli441 <tlfqj1208@gmail.com>
2023-04-24 21:14:07 +0900

    중간 저장

--- 2023.04.24/exam.js
+++ 2023.04.24/exam.js
@@ -16,12 +16,27 @@ let basicData = {
   },
 };
 
+// console.log(Object.keys(basicData).length);
+
 let elementData = ["게시판", "수업과제", "사용자", "성적", "로그아웃"];
 
 function exampleOne(object, array) {
-  // basic 객체의 children 속성이 null 데이터 타입이 할당되어있는지 판별하고
+  for (let i = 0; i < Object.keys(basicData).length; i++) {
+    if (Object.values(basicData)[i].children === null) {
+      Object.values(basicData)[i].children = elementData[i];
+      console.log(basicData);
+    }
+  }
+  // basicData 객체의 children 속성이 null 데이터 타입이 할당되어있는지 판별하고
   // null 데이터 타입이 할당되어있으면 매개변수를 각각 할당한다.
-
+  function tagMaker(tagname, parent, attribute) {
+    const element = document.createElement(tagname);
+    parent.appendChild(element);
+    if (attribute) {
+      Object.assign(element, attribute);
+    }
+    return element;
+  }
   let value;
 
   // header, main, footer HTML 태그를 생성하는 로직으로
