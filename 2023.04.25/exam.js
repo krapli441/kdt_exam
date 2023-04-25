3f61024be46584863d4981ea7fb8ce2be6ed6401
krapli441 <tlfqj1208@gmail.com>
2023-04-25 21:41:00 +0900

    오류 커밋

--- 2023.04.25/exam.js
+++ 2023.04.25/exam.js
@@ -10,57 +10,61 @@
 // 외부함수, 내부함수 구조 혹은 closure로 '함수 지향' 방식으로 조립하거나
 // 할 수 있는 자바스크립트의 '변형성'을 탐구해보자.
 
-// function exampleOne(arr, dataType, itemToFind) {
-//   if (Array.isArray(arr) === false) {
-//     console.log("배열이 아닙니다.");
-//     return false;
-//   }
+function exampleOne(arr, dataType, itemToFind) {
+  if (Array.isArray(arr) === false) {
+    console.log("배열이 아닙니다.");
+    return false;
+  }
 
-//   const isDataTypeMatched = typeof itemToFind === dataType;
-//   const isItemExist = arr.includes(itemToFind);
+  const isDataTypeMatched = typeof itemToFind === dataType;
+  const isItemExist = arr.includes(itemToFind);
 
-//   if (isDataTypeMatched === true && isItemExist === true) {
-//     console.log(`${itemToFind}는(은) 배열에 존재합니다.`);
-//     return true;
-//   } else if (isItemExist === false) {
-//     console.log(`${itemToFind}는(은) 배열에 존재하지 않습니다.`);
-//     return false;
-//   } else if (isDataTypeMatched === false) {
-//     console.log(`찾으려는 항목은 ${dataType} 데이터 타입이 아닙니다.`);
-//     return false;
-//   }
-// }
-// const basicData = [
-//   "안녕하세요",
-//   "어디선가",
-//   "입력된",
-//   "데이터를",
-//   "찾아보는",
-//   "함수입니다.",
-// ];
+  if (isDataTypeMatched === true && isItemExist === true) {
+    console.log(`${itemToFind}는(은) 배열에 존재합니다.`);
+    return true;
+  } else if (isItemExist === false) {
+    console.log(`${itemToFind}는(은) 배열에 존재하지 않습니다.`);
+    return false;
+  } else if (isDataTypeMatched === false) {
+    console.log(`찾으려는 항목은 ${dataType} 데이터 타입이 아닙니다.`);
+    return false;
+  }
+}
+const basicData = [
+  "안녕하세요",
+  "어디선가",
+  "입력된",
+  "데이터를",
+  "찾아보는",
+  "함수입니다.",
+];
 
-// console.log(exampleOne(basicData, "string", "있냐?"));
+console.log(exampleOne(basicData, "string", 11));
 
 //* 2023.04.25 문제풀이
 
 // 1. 일단 적어보고 그대로 실행.
 // 2. 기능 자체는 문제없이 잘 작동한다. 이런 걸 내가 만들 수 있을까?
 
-class ObjectFilter {
-  constructor(arr, dataType, itemToFind) {
-    this.arr = arr;
-    this.dataType = dataType;
-    this.itemToFind = itemToFind;
-  }
+// class ObjectFilter {
+//   constructor(arr, dataType, itemToFind) {
+//     this.arr = arr;
+//     this.dataType = dataType;
+//     this.itemToFind = itemToFind;
+//   }
 
-  set arr(value) {
-    if (Array.isArray(value) === true) {
-      this._arr = value;
-    } else {
-      console.log("배열이 아닙니다.");
-    }
-  }
-}
+//   set arr(value) {
+//     if (Array.isArray(value) === true) {
+//       this._arr = value;
+//     } else {
+//       console.log("배열이 아닙니다.");
+//     }
+//   }
 
-const filter = new ObjectFilter("테스트");
-console.log(filter);
+//   set dataType(value) {
+//     if ()
+//   }
+// }
+
+// const filter = new ObjectFilter(basicData);
+// console.log(filter);
