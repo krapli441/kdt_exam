fe3260715f189a6234654789630285e87755fe3b
krapli441 <tlfqj1208@gmail.com>
2023-05-02 20:19:32 +0900

    문제 작성

---
+++ 2023.05.02/exam.js
@@ -0,0 +1,12 @@
+// const add = require ('./utility-module-add.js')
+// const minus = require ('./utility-module-minus.js')
+// const minus = require ('./utility-module-divide.js')
+// const minus = require ('./utility-module-multiply.js')
+
+const a = 10;
+const b = 20;
+
+console.log(add(a, b));
+console.log(minus(a, b));
+console.log(divde(a, b));
+console.log(multiply(a, b));
