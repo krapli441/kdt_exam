947a335852571b3a72668ef706479dddbe5e4c18
krapli441 <tlfqj1208@gmail.com>
2023-05-02 21:01:04 +0900

    else문이 출력되는 이유

--- 2023.05.02/exam.js
+++ 2023.05.02/exam.js
@@ -25,24 +25,30 @@ class Calculator {
     this.cal = cal;
     this.a = a;
     this.b = b;
+    this.Calculate(cal, a, b);
   }
 
-  set cal(value) {
+  Calculate(value) {
+    let result = "";
     if (value === "add") {
       console.log("더하기 감지");
-      console.log(this.a + this.b);
+      result = this.a + this.b;
+      console.log(result);
     }
     if (value === "minus") {
       console.log("빼기 감지");
-      console.log(this.a - this.b);
+      result = this.a - this.b;
+      console.log(result);
     }
     if (value === "divide") {
       console.log("나누기 감지");
-      console.log(this.a / this.b);
+      result = this.a / this.b;
+      console.log(result);
     }
     if (value === "multiply") {
       console.log("곱하기 감지");
-      console.log(this.a * this.b);
+      result = this.a * this.b;
+      console.log(result);
     } else {
       console.log(
         "add(더하기), minus(빼기), divide(나누기), multuply(곱하기) 네가지 중 하나를 입력하셔야 합니다."
@@ -51,8 +57,7 @@ class Calculator {
   }
 }
 
-const add = new Calculator("add", 10, 20);
-console.log(add);
+console.log(new Calculator("add", 10, 20));
 
 //*2020.05.02 문제풀이
 
@@ -61,3 +66,8 @@ console.log(add);
 // 3. a에 10, b에 20이라는 수가 선언돼있으니 이걸 바꿔보면 어떨까? NaN 오류가 뜬다.
 // 4. 분명 숫자가 입력됐는데 NaN이라고 왜 뜨는 걸까?
 // 5. a와 b에 set을 추가했는데도 NaN이 뜬다.
+// 6. 이유를 찾아보니 set cal(value) 메소드가 cal 속성을 설정할 때마다
+// 사칙연산도 수행하도록 되어 있기 때문이라고 한다.
+// 7. 그렇다면 계산하는 함수를 따로 만들어야 할까?
+// 8. 계산하는 함수를 추가로 만들고 실행해봤다. 일단 계산은 된다.
+// 그런데 else문이 출력된다.
