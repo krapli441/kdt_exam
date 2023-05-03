0a0664904e8de8440d440564a4322d059e8fb088
krapli441 <tlfqj1208@gmail.com>
2023-05-03 22:23:11 +0900

    Promise 연습

--- 2023.05.03/promise.js
+++ 2023.05.03/promise.js
@@ -1,7 +1,11 @@
-let promise = new Promise(function (resolve, reject) {
-  resolve("resolve 테스트");
-});
+// let promise = new Promise(function (resolve, reject) {
+//   setTimeout(() => resolve("완료"), 1000);
+// });
 
-promise.then(function (result) {
-  console.log(result);
-});
+// // resolve 함수는 .then의 첫 번째 함수(인수)를 실행.
+// promise.then(
+//   (result) => console.log(result), // 1초 후 "완료!"를 출력
+//   (error) => console.log(error) // 실행되지 않음
+// );
+
+let promise = new Promise();
