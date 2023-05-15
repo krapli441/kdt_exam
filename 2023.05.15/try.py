fcfc3f473ef8c254e7dd933e138058b92185af28
krapli441 <tlfqj1208@gmail.com>
2023-05-15 21:15:16 +0900

    숫자인지 아닌지 확인하는 함수

--- 2023.05.15/try.py
+++ 2023.05.15/try.py
@@ -1,6 +1,8 @@
 def exampleOne(a):
     if isinstance(a, int):
-        print('hello')
+        print('숫자가 맞는 듯합니다.')
+    else:
+        print('숫자를 입력하셔야겠습니다.')
 
 
-print(exampleOne(3))
+print(exampleOne(4))
